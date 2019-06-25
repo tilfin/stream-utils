ad97cbbdaaec2dd6365dabda6c9507f46bba0952
Toshimitsu Takahashi <toshi@tilfin.com>
2019-06-25 12:17:52 +0900

    Add explanation with this.push in MapStream

--- example/example.js
+++ example/example.js
@@ -1,6 +1,18 @@
 const StreamUtils = require('../')
 
 const arrayReader = StreamUtils.readArray([1, 2, 3]);
+const oddStream = StreamUtils.map(function (num, cb) {
+  if (num % 2) {
+    cb(null, num)
+  } else {
+    cb(null)
+  }
+});
+const addCopyStream = StreamUtils.map(function (num, cb) {
+  this.push(num)
+  this.push(num)
+  cb()
+});
 const twiceStream = StreamUtils.map(function (num, cb) {
   if (typeof num === 'number') {
     cb(null, num * 2)
@@ -9,6 +21,14 @@ const twiceStream = StreamUtils.map(function (num, cb) {
   }
 });
 /* Async
+const oddStream = StreamUtils.map(async function (num) {
+  if (num % 2) return num
+});
+const addCopyStream = StreamUtils.map(function (num, ctx) {
+  ctx.push(num)
+  ctx.push(num)
+  cb()
+});
 const twiceStream = StreamUtils.map(async function (num) {
   if (typeof num === 'number') {
     return num * 2;
@@ -22,5 +42,8 @@ const arrayWriter = StreamUtils.writeArray(function (err, values) {
   console.log(values)
 });
 
-arrayReader.pipe(twiceStream);
-twiceStream.pipe(arrayWriter);
+arrayReader
+  .pipe(oddStream)       // => [1, 3]
+  .pipe(addCopyStream) // => [1, 1, 3, 3]
+  .pipe(twiceStream)     // => [2, 2, 6, 6]
+  .pipe(arrayWriter);
--- test/async.js
+++ test/async.js
@@ -27,6 +27,49 @@ describe('StreamUtils', () => {
         });
       })
 
+      context('func may return nothing', () => {
+        it('filters for each item', done => {
+          const ARRAY = [1, 2, 3];
+          const RESULT_ARRAY = [1, 3];
+
+          const arrayReader = StreamUtils.readArray(ARRAY);
+          const oddStream = StreamUtils.map(async function(number) {
+            if (number % 2) return number
+          });
+
+          arrayReader.pipe(oddStream);
+          oddStream.on('data', item => {
+            assert.equal(item, RESULT_ARRAY.shift())
+          })
+          .on('end', () => {
+            assert.isEmpty(RESULT_ARRAY);
+            done();
+          })
+          .on('error', done)
+        })
+
+        it('adds copy for each item', done => {
+          const ARRAY = [1, 2, 3];
+          const RESULT_ARRAY = [1, 1, 2, 2, 3, 3];
+
+          const arrayReader = StreamUtils.readArray(ARRAY);
+          const addCopyStream = StreamUtils.map(async function(num, ctx) {
+            ctx.push(num)
+            ctx.push(num)
+          });
+
+          arrayReader.pipe(addCopyStream);
+          addCopyStream.on('data', item => {
+            assert.equal(item, RESULT_ARRAY.shift())
+          })
+          .on('end', () => {
+            assert.isEmpty(RESULT_ARRAY);
+            done();
+          })
+          .on('error', done)
+        })
+      })
+
       context('if an error raises', () => {
         it('catches the error', done => {
           const arrayReader = StreamUtils.readArray([1]);
